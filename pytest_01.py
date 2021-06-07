5bff892755366cd795b7fdf6059891267b7944dc
Mustafa Katthawala <mustafa.katthawala2@mail.dcu.ie>
2021-06-07 16:10:16 +0530

    pytest_01.py test all pass

--- pytest_01.py
+++ pytest_01.py
@@ -1,5 +1,7 @@
 import math
 
+from main import DslMain
+
 
 def test_sqrt():
     num = 25
@@ -15,8 +17,27 @@ def test_equality():
     assert 10 == 10
 
 
+def dsl_file_check(m_dsl_obj):
+    # dsl_obj = DslMain('./dsl_files')
+    filename = m_dsl_obj.file_name
+    assert filename != ''
+
+
+def dsl_file_content(m_dsl_obj):
+    filename = m_dsl_obj.file_name
+    file_content = m_dsl_obj.load_file(filename)
+    assert file_content.find('workspace') != -1
+
+
 # pytest pytest_01.py -v --junitxml=result.xml
 if __name__ == '__main__':
-    test_sqrt()
-    test_square()
-    test_equality()
+    dsl_obj = DslMain('./dsl_files')
+    dsl_file_check(dsl_obj)
+    dsl_file_content(dsl_obj)
+
+
+
+    # test_method()
+    # test_sqrt()
+    # test_square()
+    # test_equality()
